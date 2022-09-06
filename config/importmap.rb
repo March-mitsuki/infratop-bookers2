80a8fbeccf6c80c2de2868c8f6f1f2d1ec0ed096
March-mitsuki <march_tsuki@icloud.com>
2022-09-07 03:38:03 +0900

    [add] use importmap->bootstrap,fontawesome

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# custom pin
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+pin "fontawesome", to: "https://kit.fontawesome.com/1df26a63fa.js", preload: true
