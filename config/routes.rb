fb0accf4bef07eb42ffcc901827055c4a9428386
March-mitsuki <march_tsuki@icloud.com>
2022-09-07 04:58:53 +0900

    [update] user name login, update boostrap config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root "books#home"
   devise_for :users
+
+  resources :books, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/migrate/20220906183047_devise_create_users.rb
+++ db/migrate/20220906183047_devise_create_users.rb
@@ -32,12 +32,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Custom add, username & introduction
+      t.string :name
+      t.string :introduction
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, :name,                 unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
