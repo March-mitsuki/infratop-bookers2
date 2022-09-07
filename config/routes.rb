bf7f38c24c125120e043c61724a4176cd59bdf7b
March-mitsuki <march_tsuki@icloud.com>
2022-09-07 12:14:58 +0900

    [add] user edit page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
