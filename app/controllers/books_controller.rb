52a83cd8c64941ee72be6312541560db4c9a895c
March-mitsuki <march_tsuki@icloud.com>
2022-09-08 10:31:18 +0900

    [fix] posts from books detail page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
         @user = @book.user
+        @new_book = Book.new
     end
 
     def index
