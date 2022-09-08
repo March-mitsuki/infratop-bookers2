7d43feb09c1428e0f48f83a92006b3c48772ce09
March-mitsuki <march_tsuki@icloud.com>
2022-09-08 10:20:33 +0900

    [add] favorate logic

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,8 @@ class Book < ApplicationRecord
     validates :body, presence: true, length: {
         maximum: 200
     }
+
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end
 end
